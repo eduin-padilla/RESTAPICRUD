abe4ba2af564d8703e8b94490b5fa49e17bcb880
Edwin Padilla <edwinpadillaorozco1@gmail.com>
2026-07-04 11:23:03 -0500

    revisando la conexion a la base de datos

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import express, { Express } from "express"
+import colors from 'colors'
+import router from "./route"
+import db from "./config/db"
+
+
+
+//base de datos
+
+async function connectDB() {
+
+    try{
+        await db.authenticate()
+        db.sync()
+        console.log(colors.green('conexion exitosa a la base de datos'))
+
+    }catch(error){
+        console.log(error)
+        console.log(colors.red('hubo un error al conectar a la base de datos'))
+    }
+    
+}
+
+connectDB()
+const server: Express = express()
+
+server.use('/', router)
+
+
+export default server
